eb9e6a833f18b8247ca4a8c58bb084b2b4a381b8
Brenda Butler <brendabutler@brendas-imac.home>
2020-04-20 20:30:57 -0700

    Updated app.js
    Created functions to add phrase and check letter

--- app.js
+++ app.js
@@ -17,15 +17,45 @@ const phrases = [
   'It is five oclock somewhere'
 ];
 
-function getRandomPhraseAsArray(arr) {
-  const randNum = Math.floor((Math.random() * arr.length) + 1);
-  const randomPhrase = arr[randNum];
+
+// Selects a random phrase and parse out each letter to a separate array.
+
+const getRandomPhraseAsArray = arr => {
+  const randNum = Math.floor((Math.random() * arr.length));
+  const randPhrase = arr[randNum];
   const phraseParsed = [];
-  () => {
-    for (let i=0; i<randomPhrase.length; i++) {
-      phraseParsed.push(randomPhrase[i]);
-    }
-   }
+  for (let i=0; i<randPhrase.length; i++) {
+    phraseParsed.push(randPhrase[i]);
+  }
   return phraseParsed;
 }
-console.log(getRandomPhraseAsArray(phrases));
+
+
+// Adds the random phrase to the Game Board
+
+const addPhraseToDisplay = arr => {
+  for (let i=0; i<arr.length; i++) {
+    let li = document.createElement('li');
+    li.textContent = arr[i];
+    let liRe = new RegExp('[a-zA-Z]');
+    if (liRe.test(li.textContent)) {
+      li.className = 'letter';
+    } else {
+      li.className = 'space';
+    }
+    phrase.firstElementChild.appendChild(li);
+  };
+}
+
+addPhraseToDisplay ( getRandomPhraseAsArray(phrases) );
+
+const checkLetter = btn => {
+  const letters = document.querySelectorAll('.letter');
+  for (i=0; i<letters.length; i++) {
+    if (btn === letters[i]) {
+      li.className = 'show';
+    } else {
+      return null;
+    }
+  }
+}
